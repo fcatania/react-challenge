0f0a9eb90bb819033bda2a348344094e6f8825f2
Felipe Catania <felicatania@gmail.com>
2022-11-10 19:50:54 -0300

    add ProjectList page as landing page

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ProjectList } from './pages/ProjectList/ProjectList';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProjectList />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/ProjectList/ProjectList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Typography, Button, Stack } from '@mui/material';
+import styles from './ProjectList.module.css';
+
+export const ProjectList = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Typography variant="h1" mt={2} gutterBottom fontSize={32}>
+          Project List
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">NEW PROJECT</Button>
+          <Button variant="contained">NEW USER</Button>
+        </Stack>
+      </header>
+    </div >
+  );
+}
